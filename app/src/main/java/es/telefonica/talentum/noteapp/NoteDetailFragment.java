dcc693ea8322bc4a9db477a00d3d2e67f4079cff
Adrian Diaz Mancera <adriandiazmancera@hotmail.com>
2017-01-12 14:15:05 +0100

    added code to save in defaults

--- app/src/main/java/es/telefonica/talentum/noteapp/NoteDetailFragment.java
+++ app/src/main/java/es/telefonica/talentum/noteapp/NoteDetailFragment.java
@@ -1,25 +1,95 @@
 package es.telefonica.talentum.noteapp;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class NoteDetailFragment extends Fragment {
 
+    EditText titleText;
+    EditText descriptionText;
 
     public NoteDetailFragment() {
         // Required empty public constructor
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_detail, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_note_detail, container, false);
+
+       titleText = (EditText) view.findViewById(R.id.fragment_note_detail_title_text);
+        descriptionText = (EditText) view.findViewById(R.id.fragment_note_detail_description_text);
+
+
+        return view;
     }
 
-}
+    @Override
+    public void onPause() {
+        super.onPause();
+        //save all from screen to disk : fragment is going to be destroyed
+
+        saveAllDataToDisk();
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //load data to show on screen (if any)
+
+        loadAllDataFromDisk();
+    }
+
+    private void saveAllDataToDisk() {
+        //abro el fichero para escribir
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        SharedPreferences.Editor editor = preferences.edit();
+
+
+        // leo lo que has escrito en pantalla
+        String title = titleText.getText(). toString();
+        String description = descriptionText.getText().toString();
+
+
+        // grabo eso en el fichero
+            editor.putString("NOTE_TITLE", title);
+            editor.putString("NOTE_DESCRIPTION", description);
+        editor.apply();
+    }
+
+    private void loadAllDataFromDisk() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String noteTilte = preferences.getString("NOTE_TITLE", "");
+        String noteDescription = preferences.getString("NOTE_DESCRIPTION", "");
+
+        titleText.setText(noteTilte);
+        descriptionText.setText(noteDescription);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        SharedPreferences.Editor editor = preferences.edit();
+            editor.remove("NOTE_TITLE");
+            editor.remove("NOTE_DESCRIPTION");
+        editor.apply();
+        }
+    }
+
