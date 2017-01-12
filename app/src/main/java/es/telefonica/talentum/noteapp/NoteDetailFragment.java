179d36f9d53406a5c9766407018e980bf3c66f7c
Adrian Diaz Mancera <adriandiazmancera@hotmail.com>
2017-01-12 10:09:48 +0100

    initial commit

---
+++ app/src/main/java/es/telefonica/talentum/noteapp/NoteDetailFragment.java
@@ -0,0 +1,25 @@
+package es.telefonica.talentum.noteapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NoteDetailFragment extends Fragment {
+
+
+    public NoteDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_detail, container, false);
+    }
+
+}
